95eb41d68b3c213a87b810dcd15b3760167dee39
Justin Hsie <justinhsie1001@gmail.com>
2022-06-12 17:42:27 -0700

    Fix cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -46,7 +46,7 @@ app.include_router(router)
 BACKEND_CORS_ORIGINS = [
         "http://localhost:3000",
         "http://localhost:8001",
-        "http://my-movielist-app.herokuapp.com",
+        "https://my-movielist-app.herokuapp.com",
         ]
 
 # Set all CORS enabled origins
